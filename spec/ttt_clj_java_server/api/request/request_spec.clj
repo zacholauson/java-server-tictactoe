8969030aa621e15719b3c916905a5412f4782dad
Zach Olauson <zmolauson@gmail.com>
2014-04-16 16:34:22 -0500

    wrap request in api

--- spec/ttt_clj_java_server/api/request/request_spec.clj
+++ spec/ttt_clj_java_server/api/request/request_spec.clj
@@ -1,3 +1,18 @@
 (ns ttt-clj-java-server.api.request.request-spec
   (:require [speclj.core :refer :all]
             [ttt-clj-java-server.api.request.request :refer :all]))
+
+(def mock-request
+  (reify main.requests.IRequest
+    (getParams [this]
+      (java.util.HashMap. {"move" "1"}))
+    (getCookies [this]
+      (java.util.HashMap. {"board" "---------"}))))
+
+(describe "Request"
+  (describe "#get-cookies"
+    (it "should return a map of the cookies from the request"
+      (should= {"board" "---------"} (get-cookies mock-request))))
+  (describe "#get-params"
+    (it "should return a map of the params from the request"
+      (should= {"move" "1"} (get-params mock-request)))))
--- src/ttt_clj_java_server/api/request/request.clj
+++ src/ttt_clj_java_server/api/request/request.clj
@@ -1,1 +1,7 @@
 (ns ttt-clj-java-server.api.request.request)
+
+(defn get-cookies [request]
+  (.getCookies request))
+
+(defn get-params [request]
+  (.getParams request))
